d3fb58478d159263fbad48c3034a71e09dda43e8
Antonio Bulgheroni <antonio.bulgheroni@gmail.com>
2022-07-03 15:37:43 +0200

    Cosmetic pre-commit changes

--- src/pyprivatizer/pyprivatizer.py
+++ src/pyprivatizer/pyprivatizer.py
@@ -2,29 +2,29 @@
 """
 The pyprivatizer module
 
-This module allows to download from PyPI a list of packages satisfying a 
+This module allows to download from PyPI a list of packages satisfying a
 requirement file and to copy them to a local directory with a struture that
 resemble the one of a private python index.
 
 """
 
-import subprocess
-from pathlib import Path
 import argparse
 import re
 import shutil
+import subprocess
+from pathlib import Path
 
 
 def main():
     """Execute the module.
-    
-    The module takes two input parameters, the requirements file and the 
+
+    The module takes two input parameters, the requirements file and the
     output directory where the index structure will be created.
 
     Raises
     ------
     OSError
-        An OSError will be raised if the input parameters are not found on 
+        An OSError will be raised if the input parameters are not found on
         the filesystem.
 
     Returns
@@ -46,32 +46,34 @@ def main():
 
     # check that the give parameters are valid.
     if not args.requirements_file.exists():
-        raise OSError(f'Requirement file {str(args.requirements_file)} doesn\'t exist')
+        raise OSError(
+            f'Requirement file {str(args.requirements_file)} doesn\'t exist')
     if not args.requirements_file.is_file():
-        raise OSError(f'Requirement file {str(args.requirements_file)} isn\'t a file')
+        raise OSError(
+            f'Requirement file {str(args.requirements_file)} isn\'t a file')
     if not args.output_dir.is_dir() and args.output_dir.exists():
         raise OSError(f'Output dir {str(args.output_dir)} isn\'t a directory')
-    
+
     if not args.output_dir.exists():
         args.output_dir.mkdir(parents=True)
-        
+
     # be sure to be using the latest version of pip
     command = 'python -m pip install --upgrade pip'
     print(command)
     cmd_output = subprocess.run(command.split(), capture_output=True)
-    if cmd_output.returncode :
+    if cmd_output.returncode:
         print(cmd_output.stdout.decode('utf-8'))
         print(cmd_output.stderr.decode('utf-8'))
         raise OSError('Error upgrading pip')
     else:
         print(cmd_output.stdout.decode('utf-8'))
-    
+
     tmp_dir = Path().cwd() / Path('temp')
     tmp_dir.mkdir(exist_ok=True)
     command = f'python -m pip download -r {str(args.requirements_file)} -d {str(tmp_dir)}'
     print(command)
     cmd_output = subprocess.run(command.split(), capture_output=True)
-    if cmd_output.returncode :
+    if cmd_output.returncode:
         print(cmd_output.stdout.decode('utf-8'))
         print(cmd_output.stderr.decode('utf-8'))
         raise OSError('Error upgrading pip')
@@ -84,8 +86,9 @@ def main():
             norm_name = re.sub(r"[-_.]+", "-", str(dir_name)).lower()
             (args.output_dir / Path(norm_name)).mkdir(exist_ok=True, parents=True)
             package_name = file.name
-            shutil.copy(file, (args.output_dir / Path(norm_name) / package_name))
-            
+            shutil.copy(file, (args.output_dir
+                        / Path(norm_name) / package_name))
+
     shutil.rmtree(tmp_dir)
 
     print('Packages downloaded and trasfered to destination directory')
